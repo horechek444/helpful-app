a16090542c630a0017672ed8a9f6baac9cc89d5f
Tatiana Eskina <horechek444@gmail.com>
2021-09-24 11:51:36 +0300

    feat: начало работы

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="header">
+        <span>Helpful-app</span>
+      </header>
+      <main className="main">
+
+      </main>
+      <footer className="footer">
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
